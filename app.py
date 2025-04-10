f55e9a057e864329a68ef303df0f7da2efa62b41
cnx-mscn <cmusacan@outlook.com>
2025-04-10 14:08:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from geopy.distance import geodesic
 import googlemaps
 
 # Google Maps API key
-gmaps = googlemaps.Client(key="YOUR_GOOGLE_MAPS_API_KEY")
+gmaps = googlemaps.Client(key="AIzaSyDwQVuPcON3rGSibcBrwhxQvz4HLTpF9Ws")
 
 st.set_page_config("Montaj Rota Planlayıcı", layout="wide")
 st.title("🛠️ Montaj Rota Planlayıcı ve Maliyet Hesaplayıcı")
