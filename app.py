b887b4ab40b30265eccbcff118a7ef8564a7c816
cnx-mscn <cmusacan@outlook.com>
2025-04-11 10:02:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import googlemaps
+import folium
+from streamlit_folium import st_folium
 
 # Google Maps API Key
 gmaps = googlemaps.Client(key="AIzaSyDwQVuPcON3rGSibcBrwhxQvz4HLTpF9Ws")  # <-- Buraya kendi Google API anahtarınızı yazın
@@ -90,31 +92,39 @@ for i, veri in enumerate(sorted(st.session_state.sehirler, key=lambda x: -x["one
 # Harita Oluşturma
 st.subheader("🗺️ Oluşturulan Rota Haritası")
 if st.session_state.sehirler:
-    sirali = sorted(st.session_state.sehirler, key=lambda x: -x["onem"])
-    merkez = sirali[0]['konum']
-    harita = folium.Map(location=[merkez['lat'], merkez['lng']], zoom_start=6)
+    # Konum verisi kontrolü
+    merkez = None
+    for sehir in st.session_state.sehirler:
+        if 'konum' in sehir and 'lat' in sehir['konum'] and 'lng' in sehir['konum']:
+            merkez = sehir['konum']
+            break
+    
+    if merkez:
+        harita = folium.Map(location=[merkez['lat'], merkez['lng']], zoom_start=6)
 
-    for i, sehir in enumerate(sirali):
-        folium.Marker(
-            [sehir['konum']['lat'], sehir['konum']['lng']],
-            popup=f"{i+1}. {sehir['sehir']} ({sehir['ekip']})",
-            tooltip=f"{i+1}. Durak",
-            icon=folium.DivIcon(html=f"""<div style="font-size: 12pt; color: white; background-color: blue; border-radius: 5px; padding: 2px;">{i+1}</div>""")
-        ).add_to(harita)
+        for i, sehir in enumerate(sorted(st.session_state.sehirler, key=lambda x: -x["onem"])):
+            folium.Marker(
+                [sehir['konum']['lat'], sehir['konum']['lng']],
+                popup=f"{i+1}. {sehir['sehir']} ({sehir['ekip']})",
+                tooltip=f"{i+1}. Durak",
+                icon=folium.DivIcon(html=f"""<div style="font-size: 12pt; color: white; background-color: blue; border-radius: 5px; padding: 2px;">{i+1}</div>""")
+            ).add_to(harita)
 
-        if i < len(sirali) - 1:
-            baslangic = sirali[i]['konum']
-            bitis = sirali[i+1]['konum']
-            yol = gmaps.directions(
-                (baslangic['lat'], baslangic['lng']),
-                (bitis['lat'], bitis['lng']),
-                mode="driving"
-            )
-            if yol:
-                steps = yol[0]['legs'][0]['steps']
-                rota_coords = [(step['end_location']['lat'], step['end_location']['lng']) for step in steps]
-                folium.PolyLine(rota_coords, color="blue", weight=4).add_to(harita)
+            if i < len(st.session_state.sehirler) - 1:
+                baslangic = st.session_state.sehirler[i]['konum']
+                bitis = st.session_state.sehirler[i+1]['konum']
+                yol = gmaps.directions(
+                    (baslangic['lat'], baslangic['lng']),
+                    (bitis['lat'], bitis['lng']),
+                    mode="driving"
+                )
+                if yol:
+                    steps = yol[0]['legs'][0]['steps']
+                    rota_coords = [(step['end_location']['lat'], step['end_location']['lng']) for step in steps]
+                    folium.PolyLine(rota_coords, color="blue", weight=4).add_to(harita)
 
-    st_folium(harita, width=800, height=600)
+        st_folium(harita, width=800, height=600)
+    else:
+        st.error("Konum bilgisi eksik veya hatalı. Lütfen şehirlerinizi kontrol edin.")
 else:
     st.info("Henüz şehir eklenmedi.")
